c1f9c31c79e77cb4f17225532b9d7f7f37f7bf7d
dongjun2000 <dongjun@246ha.com>
2024-11-25 09:49:35 +0800

    模版切分

--- resources/views/static_pages/contact.blade.php
+++ resources/views/static_pages/contact.blade.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>联系我们</h1>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('title', '联系我们')
+
+@section('content')
+    <h1>联系我们</h1>
+    <p>This is the contact page.</p>
+@endsection
