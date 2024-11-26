334ef510fa26170b05bd7b8472f6c1e76c569a6e
dongjun2000 <dongjun@246ha.com>
2024-11-26 10:00:34 +0800

    命令路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
   <div class="container">
-    <a href="/" class="navbar-brand">微博</a>
+    <a href="{{ route('home') }}" class="navbar-brand">微博</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-item"><a href="{{ route('help') }}" class="nav-link">帮助</a></li>
       <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
     </ul>
   </div>
