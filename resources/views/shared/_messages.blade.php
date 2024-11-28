6687cc15863d33ffcbe7d28f1727986e069e3e25
dongjun2000 <dongjun@246ha.com>
2024-11-28 15:07:36 +0800

    完成用户注册功能

---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,9 @@
+@foreach(['success', 'info', 'warning', 'danger'] as $type)
+  @if(session()->has($type))
+    <div class="flash-message">
+      <p class="alert alert-{{ $type }}">
+        {{ session()->get($type) }}
+      </p>
+    </div>
+  @endif
+@endforeach
