fe3ac42e0bd2257b709581bc3c0960e93f7cf66d
dongjun2000 <dongjun@246ha.com>
2024-11-29 19:44:29 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -18,15 +18,24 @@
 
                   <div class="mb-3">
                       <label for="email">邮箱：</label>
-                      <input type="text" name="email" class="form-control" value="{{ old('email') }}" autofocus>
+                      <input type="text" name="email" class="form-control" value="{{ old('email') }}">
                   </div>
                   <div class="mb-3">
                       <label for="password">密码：</label>
                       <input type="password" name="password" id="password" class="form-control">
                   </div>
 
+                  <div class="mb-3">
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" name="remember" id="remember" value="1">
+                      <label class="form-check-label" for="remember">记住我</label>
+                    </div>
+                  </div>
+
                   <button type="submit" class="btn btn-primary">登录</button>
               </form>
+              <hr>
+              <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
             </div>
         </div>
     </div>
